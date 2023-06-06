4c30e1d0526f5bd7af9086eaa5ce4f7c423a94b1
charmer3 <125043847+charmer3@users.noreply.github.com>
2023-06-06 16:35:07 +0900

    update

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+a = int(input("번호를 입력하세요: "))
+print(a)
+
+if a==1:
+    print("1이 출력되었습니다.")
+
+elif a==2:
+    print("2가 출력되었습니다")
+
+elif a==3:
+    print("은준이는 바보입니다.")
+
+else:
+    print("적절한 숫자가 아닙니다.")
